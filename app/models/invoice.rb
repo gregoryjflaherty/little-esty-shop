8bce2999eae0aa7b5f92545254f8bffad90510aa
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-18 20:22:12 -0500

    Revert "Setup"

--- app/models/invoice.rb
+++
@@ -1,8 +0,0 @@
-class Invoice < ApplicationRecord
-  enum status: ["cancelled", "completed", "in progress"]
-
-  belongs_to :customer
-  has_many :transactions
-  has_many :invoice_items
-  has_many :items, through: :invoice_items
-end
--- spec/models/merchant_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Merchant, type: :model do
-  describe 'relationships' do
-    it { should have_many :items}
-  end 
-end
