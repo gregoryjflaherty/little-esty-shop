504278a2a1f0eb5ed309adb45adfac50e5ba0c27
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-19 14:05:00 -0700

    Gems, models, and model spec added

---
+++ app/models/invoice.rb
@@ -0,0 +1,9 @@
+class Invoice < ApplicationRecord
+  enum status: ["cancelled", "completed", "in progress"]
+
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+
+end
