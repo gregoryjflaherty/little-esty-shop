ad69ea494fac53b848c34b02e69381c182a099ee
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-19 19:59:20 -0500

    Test: create tests for child reelationships- invoices, transactions, customers

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items}
+    it { should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:transactions).through(:invoices)}
+    it { should have_many(:customers).through(:transactions)}
   end
 end
