65984798b00938d8a7aa0df01b3098270b9ed433
Ian Ross <ianrossdrums@gmail.com>
2022-02-20 16:26:03 -0700

    test: item name link takes to its show page

--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -7,10 +7,19 @@ RSpec.describe 'User story 34' do
     @item1 = @merchant1.items.create!(name: "Ferret Food", unit_price: 20)
     @item2 = @merchant1.items.create!(name: "Ferret Leash", unit_price: 20)
     @item3 = @merchant2.items.create!(name: "Ferret Shampoo", unit_price: 20)
+  end
+
+  it 'item name on index page goes to item show page' do
+    visit "/merchants/#{@merchant1.id}/items" #merchant items index
+
+    click_on_link "#{@item1.name}"
+
+    expect(page).to have_current_path("/merchants/#{@merchant1.id}/items")
+  end
+
+  it 'shows every items attributes' do
     visit "/merchants/#{@merchant1.id}/items"
 
-  end
-  it 'shows every items attributes' do
     expect(page).to have_content(@item1.name)
     expect(page).to have_content(@item1.description)
     expect(page).to have_no_content(@item1.unit_price)
