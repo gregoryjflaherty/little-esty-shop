305b73b2b7021fddd93dfac74f8cfd9a5b756dd2
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-21 09:24:37 -0500

    Feat: add transaction association and test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Transaction, type: :model do
 
   describe 'validations' do
     it { should define_enum_for(:result).with_values([:failed, :success]) }
+    it { should have_many(:customers).through(:invoice)}
+    it { should have_many(:invoice_items).through(:invoice)}
+    it { should have_many(:items).through(:invoice_items)}
+    it { should have_many(:merchants).through(:items)}
   end
 end
