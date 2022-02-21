c294a743366e1ed2688a35e1844d24e5326a5b41
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-21 09:24:52 -0500

    Feat: create show and test feature for top_five_customers

--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -3,10 +3,72 @@ require 'rails_helper'
 RSpec.describe 'User story 38' do
   before(:each) do
     @nike = Merchant.create!(name: "Nike")
+
+    @shoe_1 = Item.create(name: 'shoe_1', merchant_id: @nike.id)
+
+    @customer_1 = Customer.create!(first_name: 'One',last_name:'Customer' )
+    @customer_2 = Customer.create!(first_name: 'Two',last_name:'Customer' )
+    @customer_3 = Customer.create!(first_name: 'Three' ,last_name:'Customer' )
+    @customer_4 = Customer.create!(first_name: 'Four',last_name:'Customer' )
+    @customer_5 = Customer.create!(first_name: 'Five',last_name:'Customer' )
+    @customer_6 = Customer.create!(first_name: 'Six',last_name:'Customer' )
+
+    @invoice_1 = Invoice.create(customer_id: @customer_1.id, status: 1)
+    @invoice_2 = Invoice.create(customer_id: @customer_2.id, status: 1)
+    @invoice_3 = Invoice.create(customer_id: @customer_3.id, status: 1)
+    @invoice_4 = Invoice.create(customer_id: @customer_4.id, status: 1)
+    @invoice_5 = Invoice.create(customer_id: @customer_5.id, status: 1)
+    @invoice_6 = Invoice.create(customer_id: @customer_6.id, status: 1)
+
+    @invoice_item_1 = InvoiceItem.create(invoice_id: @invoice_1.id, item_id: @shoe_1.id, status: 2)
+    @invoice_item_2 = InvoiceItem.create(invoice_id: @invoice_2.id, item_id: @shoe_1.id, status: 1)
+    @invoice_item_3 = InvoiceItem.create(invoice_id: @invoice_3.id, item_id: @shoe_1.id, status: 1)
+    @invoice_item_4 = InvoiceItem.create(invoice_id: @invoice_4.id, item_id: @shoe_1.id, status: 1)
+    @invoice_item_5 = InvoiceItem.create(invoice_id: @invoice_5.id, item_id: @shoe_1.id, status: 1)
+    @invoice_item_6 = InvoiceItem.create(invoice_id: @invoice_6.id, item_id: @shoe_1.id, status: 1)
+
+
+    @transaction_1 = Transaction.create(invoice_id: @invoice_item_1.invoice.id, result: 0)
+    @transaction_2 = Transaction.create(invoice_id: @invoice_item_2.invoice.id, result: 1)
+    @transaction_3 = Transaction.create(invoice_id: @invoice_item_3.invoice.id, result: 1)
+    @transaction_4 = Transaction.create(invoice_id: @invoice_item_4.invoice.id, result: 1)
+    @transaction_5 = Transaction.create(invoice_id: @invoice_item_5.invoice.id, result: 1)
+    @transaction_6 = Transaction.create(invoice_id: @invoice_item_6.invoice.id, result: 1)
+
     visit "/merchants/#{@nike.id}/dashboard"
   end
 
-  xit 'shows top 5 customers ' do
-    
+  it 'shows top 5 customers ' do
+    expect(page).to have_content('Top Customers')
+
+    within '#customer-0' do
+      expect(page).to have_content(@customer_2.first_name)
+      expect(page).to have_content(@customer_2.last_name)
+      expect(page).to have_content(1)
+    end
+
+    within '#customer-1' do
+      expect(page).to have_content(@customer_3.first_name)
+      expect(page).to have_content(@customer_3.last_name)
+      expect(page).to have_content(1)
+    end
+
+    within '#customer-2' do
+      expect(page).to have_content(@customer_4.first_name)
+      expect(page).to have_content(@customer_4.last_name)
+      expect(page).to have_content(1)
+    end
+
+    within '#customer-3' do
+      expect(page).to have_content(@customer_5.first_name)
+      expect(page).to have_content(@customer_5.last_name)
+      expect(page).to have_content(1)
+    end
+
+    within '#customer-4' do
+      expect(page).to have_content(@customer_6.first_name)
+      expect(page).to have_content(@customer_6.last_name)
+      expect(page).to have_content(1)
+    end
   end
 end
