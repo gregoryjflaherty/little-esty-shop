514a88459576d59fdcbeaea5895ed1ec9bfe3596
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-21 12:31:01 -0700

    Debug: FIx route order, add missing ends from merge.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     resources :dashboard, controller: 'merchant_dashboard'
   end
 
-  get 'merchants/:merchant_id/:item_id', to: 'merchant_items#show'
   get '/merchants/:merchant_id/invoices',to: 'merchant_invoices#index'
+  get 'merchants/:merchant_id/:item_id', to: 'merchant_items#show'
 
 
   #get 'merchants/:merchant_id/dashboard', to: 'dashboard#show'
--- spec/features/merchant_items/show_spec.rb
+++ spec/features/merchant_items/show_spec.rb
@@ -70,29 +70,31 @@ RSpec.describe 'User story 38' do
       expect(page).to have_content(@customer_6.last_name)
       expect(page).to have_content(1)
     end
-    
-describe 'User story 34' do
-  before(:each) do
-    @merchant1 = Merchant.create!(name: "Pabu")
-    @merchant2 = Merchant.create!(name: "Loki")
-    @item1 = @merchant1.items.create!(name: "Ferret Food", unit_price: 20)
-    @item2 = @merchant1.items.create!(name: "Ferret Leash", unit_price: 20)
-    @item3 = @merchant2.items.create!(name: "Ferret Shampoo", unit_price: 20)
   end
 
-  it 'item name on index page goes to item show page' do
-    visit "/merchants/#{@merchant1.id}/items" #merchant items index
+  describe 'User story 34' do
+    before(:each) do
+      @merchant1 = Merchant.create!(name: "Pabu")
+      @merchant2 = Merchant.create!(name: "Loki")
+      @item1 = @merchant1.items.create!(name: "Ferret Food", unit_price: 20)
+      @item2 = @merchant1.items.create!(name: "Ferret Leash", unit_price: 20)
+      @item3 = @merchant2.items.create!(name: "Ferret Shampoo", unit_price: 20)
+    end
 
-    click_link "#{@item1.name}"
+    it 'item name on index page goes to item show page' do
+      visit "/merchants/#{@merchant1.id}/items" #merchant items index
 
-    expect(page).to have_current_path("/merchants/#{@merchant1.id}/#{@item1.id}")
-  end
+      click_link "#{@item1.name}"
 
-  it 'shows every items attributes' do
-    visit "/merchants/#{@merchant1.id}/#{@item1.id}"
+      expect(page).to have_current_path("/merchants/#{@merchant1.id}/#{@item1.id}")
+    end
 
-    expect(page).to have_content(@item1.name)
-    expect(page).to have_content(@item1.description)
-    expect(page).to have_content(@item1.unit_price)
+    it 'shows every items attributes' do
+      visit "/merchants/#{@merchant1.id}/#{@item1.id}"
+
+      expect(page).to have_content(@item1.name)
+      expect(page).to have_content(@item1.description)
+      expect(page).to have_content(@item1.unit_price)
+    end
   end
 end
