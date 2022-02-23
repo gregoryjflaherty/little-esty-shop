c2ef7c8e50b0373850702efd032f41adca4e975a
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-22 18:19:24 -0700

    Feat: us36 method complete

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -6,4 +6,12 @@ class InvoiceItem < ApplicationRecord
   has_many :merchants, through: :item
   has_many :customers, through: :invoice
   has_many :transactions, through: :invoice
+
+  def self.invoice_created_sorted(merchant)
+    binding.pry
+    joins(:merchants).where("merchant_id = ?", merchant.id)
+      .where("status = ?", 0).order(created_at: :asc)
+    #can change number if we want to reference another status, current status is "packaged"
+    # where("status = ?", 0).order(created_at: :asc)
+  end
 end
