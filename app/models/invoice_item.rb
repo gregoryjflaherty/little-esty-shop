624c4bfd8d378fa8c2ba1f341f94eff1d03036c8
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-24 11:48:53 -0700

    Refactor: Remove unnecessary active record method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,11 +7,4 @@ class InvoiceItem < ApplicationRecord
   has_many :customers, through: :invoice
   has_many :transactions, through: :invoice
 
-  def self.invoice_created_sorted(merchant)
-    binding.pry
-    joins(:merchants).where("merchant_id = ?", merchant.id)
-      .where("status = ?", 0).order(created_at: :asc)
-    #can change number if we want to reference another status, current status is "packaged"
-    # where("status = ?", 0).order(created_at: :asc)
-  end
 end
