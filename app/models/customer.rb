03b7e89899b5b3d87d40df23e3c81ed2c38d3199
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-26 12:34:07 -0700

    Refactor: Move top_five_customers to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,4 +12,13 @@ class Customer < ApplicationRecord
   def full_name
     (self.first_name) + " " + (self.last_name)
   end
+
+  def self.top_five_customers
+    joins(:invoices, :transactions)
+      .where('transactions.result = ?', 1)
+      .group('customers.id')
+      .select('customers.*, count(*) AS transaction_count')
+      .order('transaction_count DESC')
+      .limit(5)
+  end
 end
