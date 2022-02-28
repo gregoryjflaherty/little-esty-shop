f194d1f80f1f40d2fcac9d4965e9170004d2396d
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-28 13:55:51 -0700

    Add model methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,4 +10,14 @@ class Invoice < ApplicationRecord
   def self.incomplete_invoices
     joins(:invoice_items).where('invoice_items.status != ?', 2).distinct
   end
+
+  def creation_date_formatted
+    date = self.created_at
+    date.strftime("%A, %B %d, %Y")
+  end
+
+  def items_by_merchant(merchant_id)
+    items.joins(:invoice_items)
+      .where('merchant_id = ?', merchant_id)
+  end
 end
