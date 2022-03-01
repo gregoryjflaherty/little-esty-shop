d5f26dfb4ae3a3b06ade7a571a7b7316722e0a36
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-28 19:56:01 -0700

    Add test for invoice item attributes

--- spec/features/merchant_invoices/show_spec.rb
+++ spec/features/merchant_invoices/show_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe 'merchant invoice show page' do
     @nike = Merchant.create!(name: "Nike")
 
     @shoe_1 = Item.create(name: 'running shoe', description: 'shoe for running', unit_price: 100, merchant_id: @nike.id)
+    @shoe_2 = Item.create(name: 'trail running shoe', description: 'shoe for running on trails', unit_price: 200, merchant_id: @nike.id)
 
     @customer_1 = Customer.create!(first_name: 'Bob',last_name:'Vance' )
 
     @invoice_1 = Invoice.create(customer_id: @customer_1.id, status: 1)
-
+    @invoice_2 = Invoice.create(customer_id: @customer_1.id, status: 1)
+    
     @invoice_item_1 = InvoiceItem.create(quantity: 2, unit_price: 100, invoice_id: @invoice_1.id, item_id: @shoe_1.id, status: 2)
-    @invoice_item_2 = InvoiceItem.create(quantity: 2, unit_price: 100, invoice_id: @invoice_1.id, item_id: @shoe_1.id, status: 1)
-    @invoice_item_3 = InvoiceItem.create(quantity: 2, unit_price: 100, invoice_id: @invoice_1.id, item_id: @shoe_1.id, status: 1)
+    @invoice_item_2 = InvoiceItem.create(quantity: 2, unit_price: 200, invoice_id: @invoice_2.id, item_id: @shoe_2.id, status: 1)
 
     visit merchant_invoice_path(@nike.id, @invoice_1.id)
   end 
@@ -27,5 +28,13 @@ RSpec.describe 'merchant invoice show page' do
 
   it 'shows invoice #created_at date formatted in DAY, MM DD, YYYY' do
     expect(page).to have_content(@invoice_1.creation_date_formatted)
+  end
+
+  it 'shows invoice item attributes only for that invoice' do
+    expect(page).to have_content(@shoe_1.name)
+    expect(page).to have_content(@invoice_item_1.unit_price)
+    expect(page).to have_content(@invoice_item_1.quantity)
+    expect(page).to have_content(@invoice_item_1.status)
+    expect(page).to_not have_content(@shoe_2.name)
   end
 end
