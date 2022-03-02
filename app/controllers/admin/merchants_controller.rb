1f7ac8620c74f5992e7865b44c2a7ea9c6e51bf9
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-02 09:38:37 -0700

    Feat: Update contoller to get all params

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -13,18 +13,17 @@ class Admin::MerchantsController < AdminController
 
   def update
     @merchant = Merchant.find(params[:id])
-    
-    if merchant_params[:status]
+    if params[:status]
       @merchant.change_enabled_status(params[:status])
       redirect_to admin_merchants_path
     else
       @merchant.update!(merchant_params)
-      redirect_to admin_merchant_path(merchant),  notice: "Merchant Successfully Updated"
+      redirect_to admin_merchant_path(@merchant),  notice: "Merchant Successfully Updated"
     end
   end
 
   private
   def merchant_params
-    params.require(:merchant).permit(:name, :status)
+    params.permit(:name, :status)
   end
 end
