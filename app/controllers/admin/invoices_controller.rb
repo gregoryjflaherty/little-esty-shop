eb94a8a5816cefeffe42c6723d7ae8c28dd2b2c5
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-02 12:58:20 -0700

    Add update method to include status change

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,4 +6,17 @@ class Admin::InvoicesController < AdminController
   def show
     @invoice = Invoice.find(params[:id])
   end
+
+  def update
+    @invoice = Invoice.find(params[:id])
+    if params[:status]['cancelled']
+      @invoice.update(status: 0)
+    elsif params[:status]['completed']
+      @invoice.update(status: 1)
+    elsif params[:status]['in progress']
+      @invoice.update(status: 2)
+    end
+
+    redirect_to admin_invoice_path(params[:id])
+  end 
 end
