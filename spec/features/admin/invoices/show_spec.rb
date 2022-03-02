e48a7922ea291db8c3cd3184c0152d8ca974a660
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-02 13:17:10 -0500

    Test: Add test for us 7

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -3,22 +3,47 @@ require 'rails_helper'
 RSpec.describe 'admin_invoices show page' do
   before(:each) do
     @nike = Merchant.create!(name: "Nike")
-    @af_one = @nike.items.create!(name: "Air Force One", unit_price: 120)
+
+    @af_one = @nike.items.create!(name: "Air Force One", unit_price: 12000)
+    @boot = @nike.items.create!(name: "Nike Boot", unit_price: 20000)
 
     @customer_1 = Customer.create!(first_name:'Fake',last_name:'Customer' )
 
     @invoice_1 = Invoice.create(customer_id: @customer_1.id, status: 1, created_at: '2022-01-01')
+
+    @invoice_item_1 = InvoiceItem.create(invoice_id: @invoice_1.id, item_id: @af_one.id, status: 2, unit_price: 12000, quantity: 1)
+    @invoice_item_2 = InvoiceItem.create(invoice_id: @invoice_1.id, item_id: @boot.id, status: 1, unit_price: 20000, quantity: 1)
   end
 
   describe 'User Story 8' do
     it 'goes to invoice show page which include id, status, created_at, and full name' do
       visit admin_invoice_path(@invoice_1)
       expect(current_path).to eq(admin_invoice_path(@invoice_1))
-      save_and_open_page
+
       expect(page).to have_content("Invoice: #{@invoice_1.id}")
       expect(page).to have_content("Invoice status: #{@invoice_1.status}")
       expect(page).to have_content("Created at: #{@invoice_1.creation_date_formatted}")
       expect(page).to have_content("For: #{@customer_1.full_name}")
+    end
+  end
+
+#   As an admin
+# When I visit an admin invoice show page
+# Then I see all of the items on the invoice including:
+#
+# Item name
+# The quantity of the item ordered
+# The price the Item sold for
+# The Invoice Item status
+
+  describe 'User Story 7' do
+    it 'invoice show shows all items on invoice including name, quantity, price & invoice_item status' do
+      visit admin_invoice_path(@invoice_1)
+      expect(current_path).to eq(admin_invoice_path(@invoice_1))
+
+      within "div.items" do
+        expect(page).to have_content()
+      end
     end
   end
 end
