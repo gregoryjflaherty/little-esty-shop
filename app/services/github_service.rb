09eec7781e678ad716e2e869e6f49b438c11fd5e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-03 09:37:04 -0500

    Bootstrap table/ api

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,7 +2,13 @@ require 'httparty'
 require 'pry'
 
 class GitHubService
-  response = HTTParty.get('https://api.github.com/repos/gregoryjflaherty/little-esty-shop')
-  parsed = JSON.parse(response.body, symbolize_names: true)
-  binding.pry
+
+  def get_body
+    response = HTTParty.get('https://api.github.com/repos/gregoryjflaherty/little-esty-shop/pulls', query: {state: 'all'} )
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+  end
 end
+
+service = GitHubService.new
+service.get_body
